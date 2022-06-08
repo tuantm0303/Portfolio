595caa741e00b2db62c32f341dc54b67a97bcf77
tuantmph13096 <tuantmph13096@fpt.edu.vn>
2022-06-08 22:42:51 +0700

    style: outsides layout

---
+++ src/app/pages/outside/works-page/works-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-works-page',
+  templateUrl: './works-page.component.html',
+  styleUrls: ['./works-page.component.css']
+})
+export class WorksPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

388110ec090d7702276e563fbd515f9c0b17634c
tuantmph13096 <tuantmph13096@fpt.edu.vn>
2022-06-10 22:20:39 +0700

    service category-post & category-project

--- src/app/services/category-post.service.ts
+++ src/app/services/category-post.service.ts
@@ -1,9 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { categoryPost } from '../models';
+
+const apiUrl = 'http://localhost:3000/categoriesPosts';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CategoryPostService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getAll() {
+    return this.http.get<categoryPost[]>(apiUrl);
+  }
+
+  getOne(id: categoryPost) {
+    return this.http.get<categoryPost[]>(`${apiUrl}/${id}`);
+  }
+
+  remove(id: number | string) {
+    return this.http.delete<categoryPost[]>(`${apiUrl}/${id}`);
+  }
+
+  create(cate: categoryPost) {
+    return this.http.post<categoryPost[]>(`${apiUrl}`, cate);
+  }
+
+  update(cate: categoryPost) {
+    return this.http.put<categoryPost[]>(`${apiUrl}/${cate.id}`, cate);
+  }
 }
--- src/app/services/category-project.service.ts
+++ src/app/services/category-project.service.ts
@@ -1,9 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { CategoryProject } from '../models';
+
+const apiUrl = 'http://localhost:3000/categoriesProjects';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CategoryProjectService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getAll() {
+    return this.http.get<CategoryProject[]>(apiUrl);
+  }
+
+  getOne(id: CategoryProject) {
+    return this.http.get<CategoryProject>(`${apiUrl}/${id}`);
+  }
+
+  remove(id: number | string) {
+    return this.http.delete<CategoryProject>(`${apiUrl}/${id}`);
+  }
+
+  create(cate: CategoryProject) {
+    return this.http.post<CategoryProject>(`${apiUrl}`, cate);
+  }
+
+  update(cate: CategoryProject) {
+    return this.http.put<CategoryProject>(`${apiUrl}/${cate.id}`, cate);
+  }
 }
